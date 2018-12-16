28fbb2d717a48e1f80f1a96780bd7542d86b1efa
StackTc <353071437@qq.com>
2018-12-16 19:24:23 +0800

    :fire: JAVA连接ES查询数据

--- src/test/java/com/qinwell/elasticsearch/ElasticsearchApplicationTests.java
+++ src/test/java/com/qinwell/elasticsearch/ElasticsearchApplicationTests.java
@@ -1,12 +1,22 @@
 package com.qinwell.elasticsearch;
 
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class ElasticsearchApplicationTests {
 
 	@Test
@@ -15,6 +25,25 @@ public class ElasticsearchApplicationTests {
 		 * testa
 		 */
 		System.out.println("test");
+		log.info("test");
+	}
+
+	@Test
+	public void searchTest() throws UnknownHostException {
+		//选择es中的集群,集群默认elasticsearch
+		Settings settings = Settings.builder().put("cluster.name","elasticsearch").build();
+		//创建访问客户端,需要知道具体节点的ip跟端口号
+		InetAddress inetAddress = InetAddress.getByName("39.104.207.251");
+		//端口用9300 ，
+		// 9300端口是使用tcp客户端连接使用的端口；
+		//9200端口是通过http协议连接es使用的端口；
+		TransportAddress transportAddress = new TransportAddress(inetAddress,9300);
+		TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(transportAddress);
+		//数据查询
+		GetResponse getResponse = client.prepareGet("stacktc","test","1").execute().actionGet();
+		System.out.println("data:" + getResponse.getSourceAsString());
+		log.info("data:{}" , getResponse.getSourceAsString());
+		client.close();
 	}
 
 }
